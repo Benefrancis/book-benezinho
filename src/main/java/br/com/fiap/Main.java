b8809e4e3beb5fcb26e782964f94a0f1ad2a79a8
benefrancis <benefrancis@gmail.com>
2023-09-20 21:01:48 -0300

    Criando o servidor HTTP da API

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,35 +1,18 @@
 package br.com.fiap;
 
-import br.com.fiap.domain.entity.Pessoa;
-import br.com.fiap.domain.entity.PessoaFisica;
-import br.com.fiap.domain.entity.PessoaJuridica;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
-
 public class Main {
-
     public static final String BASE_URI = "http://localhost";
-
 
     public static HttpServer startServer(){
        final ResourceConfig  rc = new ResourceConfig();
        return null;
     }
 
-
     public static void main(String[] args) {
 
-
-
-
     }
-
 
 }
