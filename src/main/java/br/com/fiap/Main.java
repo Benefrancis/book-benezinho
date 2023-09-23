a33e9074d7182d0b404a7067e763f97eac896831
Benefrancis <benefrancis@gmail.com>
2023-09-22 21:35:53 -0300

    Server definido

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,18 +1,30 @@
 package br.com.fiap;
 
 import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-public class Main {
-    public static final String BASE_URI = "http://localhost";
+import java.io.IOException;
+import java.net.URI;
 
-    public static HttpServer startServer(){
-       final ResourceConfig  rc = new ResourceConfig();
-       return null;
+public class Main {
+    public static final String BASE_URI = "http://localhost/";
+    public static HttpServer startServer() {
+        final ResourceConfig rc = new ResourceConfig()
+                .packages( "br.com.fiap.domain.resources" );
+        return GrizzlyHttpServerFactory.createHttpServer( URI.create( BASE_URI ), rc );
     }
 
     public static void main(String[] args) {
-
+        final HttpServer server = startServer();
+        System.out.println(
+                String.format( "Bookstore app started with endpoints available " +
+                        "as %s%nHit Ctrl-C to stop it....", BASE_URI )
+        );
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            throw new RuntimeException( e );
+        }
     }
-
 }
